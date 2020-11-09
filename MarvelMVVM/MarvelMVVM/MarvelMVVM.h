dab09fdf349887ec3fe6381fbd0cc01c30a88d8a
Rodrigo Alejandro Velazquez Alcantara <rodrigo.velazquez@globant.com>
2020-11-09 13:52:49 -0600

    Add Framework

---
+++ MarvelMVVM/MarvelMVVM/MarvelMVVM.h
@@ -0,0 +1,19 @@
+//
+//  MarvelMVVM.h
+//  MarvelMVVM
+//
+//  Created by Rodrigo Alejandro Velazquez Alcantara on 09/11/20.
+//  Copyright © 2020 Rodrigo Alejandro Velazquez Alcantara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MarvelMVVM.
+FOUNDATION_EXPORT double MarvelMVVMVersionNumber;
+
+//! Project version string for MarvelMVVM.
+FOUNDATION_EXPORT const unsigned char MarvelMVVMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MarvelMVVM/PublicHeader.h>
+
+
