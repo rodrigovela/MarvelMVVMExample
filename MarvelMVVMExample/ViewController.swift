97cb175a82d7836c06c26c6e54448e1a16163b53
Rodrigo Alejandro Velazquez Alcantara <rodrigo.velazquez@globant.com>
2020-11-09 11:43:24 -0600

    Initial Commit

---
+++ MarvelMVVMExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelMVVMExample
+//
+//  Created by Rodrigo Alejandro Velazquez Alcantara on 09/11/20.
+//  Copyright © 2020 Rodrigo Alejandro Velazquez Alcantara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
